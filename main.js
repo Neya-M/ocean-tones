fb3cdf1f729e6390f3fb02775ac3a5bc971c50fe
Neya <neya@tmps-Mac-mini-2.local>
2025-09-27 13:16:51 -0700

    draw sine waves

--- main.js
+++ main.js
@@ -14,14 +14,24 @@ oscillator.type = "sine";
 oscillator.start();
 gainNode.gain.value = 0;
 
+//define canvas variables
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d"); 
+var width = ctx.canvas.width;
+var height = ctx.canvas.height;
+
+freq = pitch / 10000;
+var amplitude = 40;
+var interval = null;
+
 notenames = new Map();
-mapName.set("A", 440);
-mapName.set("B", 493.9);
-mapName.set("C", 261.6);
-mapName.set("D", 293.7);
-mapName.set("E", 329.6);
-mapName.set("F", 349.2);
-mapName.set("G", 392);
+notenames.set("A", 440);
+notenames.set("B", 493.9);
+notenames.set("C", 261.6);
+notenames.set("D", 293.7);
+notenames.set("E", 329.6);
+notenames.set("F", 349.2);
+notenames.set("G", 392);
 
 function frequency(pitch) {
 	gainNode.gain.setValueAtTime(100, audioCtx.currentTime)
@@ -34,5 +44,28 @@ gainNode.gain.value = 0;
 
 function handle() {
 	var userinput = String(input.value)
-   	frequency(mapName.get(userinput));
+   	frequency(notenames.get(userinput));
+	drawWave()
+}
+
+var counter = 0;
+function drawWave() {
+	ctx.clearRect(0, 0, width, height);
+	x = 0;
+	y = height/2;
+	ctx.moveTo(x, y); 
+	ctx.beginPath();
+	counter = 0;
+	interval = setInterval(line, 20);
+}
+
+function line() {
+	y = height/2 + (amplitude * Math.sin(2 * Math.PI * freq * x));
+	ctx.lineTo(x, y);
+	ctx.stroke();
+	x += 1;
+	counter++;
+	if(counter > 50) {
+		clearInterval(interval);
+  	}
 }
