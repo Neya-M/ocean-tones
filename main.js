86364c34155b789a1a9876a051fa2e3bed2dbe71
Neya <neya@tmps-Mac-mini-2.local>
2025-09-27 12:32:45 -0700

    swapped numbers for letters

--- main.js
+++ main.js
@@ -14,6 +14,15 @@ oscillator.type = "sine";
 oscillator.start();
 gainNode.gain.value = 0;
 
+notenames = new Map();
+mapName.set("A", 440);
+mapName.set("B", 493.9);
+mapName.set("C", 261.6);
+mapName.set("D", 293.7);
+mapName.set("E", 329.6);
+mapName.set("F", 349.2);
+mapName.set("G", 392);
+
 function frequency(pitch) {
 	gainNode.gain.setValueAtTime(100, audioCtx.currentTime)
 	oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime)
@@ -24,5 +33,6 @@ audioCtx.resume();
 gainNode.gain.value = 0;
 
 function handle() {
-   frequency(input.value);
+	var userinput = String(input.value)
+   	frequency(mapName.get(userinput));
 }
