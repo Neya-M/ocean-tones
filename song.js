da6bbb7f74ccf5c52a2ffdd3bd5286cb10c6a38a
Neya <neya@tmps-Mac-mini-2.local>
2025-10-27 17:37:37 -0700

    buttons sort of work

--- song.js
+++ song.js
@@ -20,34 +20,58 @@ var height = ctx.canvas.height;
 const wave = new Image();
 
 var amplitude = 20;
-var phase = 0;
+var progress = 0;
 var freq = 0;
 var pitch = 0;
+var notes = [];
+let frequencies = new Map();
+frequencies.set("C3", 130.81);
+frequencies.set("D3", 146.83);
+frequencies.set("E3", 164.81);
+frequencies.set("F3", 174.61);
+frequencies.set("G3", 196);
+frequencies.set("A3", 220);
+frequencies.set("B3", 246.94);
+frequencies.set("C4", 261.63);
+frequencies.set("D4", 293.66);
+frequencies.set("E4", 329.63);
+frequencies.set("F4", 349.23);
+frequencies.set("G4", 392);
+frequencies.set("A4", 440);
+frequencies.set("B4", 493.88);
 
 
 pitch = 30;
 
 function frequency() {
-	gainNode.gain.setValueAtTime(amplitude, audioCtx.currentTime)
-	oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime)
+	gainNode.gain.setValueAtTime(amplitude, audioCtx.currentTime);
+	oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
 	freq = pitch / 10000;
 }
 
 function stop() {
-	gainNode.gain.setValueAtTime(0, audioCtx.currentTime)
+	gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
 }
 
 audioCtx.resume();
 gainNode.gain.value = 0;
 
-function handle() {
-	frequency()
+function play() {
+	for (let i = 0; i < notes.length; i++) {
+		frequency(notes[i]);
+	}
+}
+
+function addNote(note) {
+	pitch = frequencies[note];
+	notes.push(pitch)
+	frequency();
 }
 
 function animate() {
-  drawWave();
-  phase -= 0.1;
-  requestAnimationFrame(animate);
+  	drawWave();
+  	requestAnimationFrame(animate);
+	progress += 1;
 }
 animate();
 
@@ -60,15 +84,15 @@ function drawWave() {
 	gradient.addColorStop(1, `rgb(30, 144, 255)`);
 	ctx.fillStyle = gradient;
     	for (let x = 0; x < width; x++) {
-        	let y = height/3 + (amplitude/2 * Math.sin(2 * Math.PI * x * freq + phase));
+        	let y = height/3 + (amplitude/2 * Math.sin(2 * Math.PI * x * freq));
         	if (x === 0) {
-            	ctx.moveTo(x, y);
+            	ctx.moveTo(x, y + progress);
         	} else {
-            	ctx.lineTo(x, y);
+            	ctx.lineTo(x, y + progress);
         	}
     	}
-	ctx.lineTo(width - 1, height);
-    	ctx.lineTo(0, height);
+	ctx.lineTo(width - 1, 0);
+    	ctx.lineTo(0, 0);
     	ctx.closePath();
 	ctx.fill();
 }
